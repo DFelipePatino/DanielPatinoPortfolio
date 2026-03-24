aab45c7c996e7b48ea54a9fabd618ceed15030b6
Daniel Patino <dafepa10c29@gmail.com>
2026-03-24 17:07:36 -0500

    about edited

--- src/OOPData/About-data.jsx
+++ src/OOPData/About-data.jsx
@@ -8,7 +8,7 @@ export const ABOUT = {
         title: "Experience",
         description: "3+ year\nFull Stack Development",
     },
-    description: "I'm a Full Stack Developer with expertise in JavaScript, React, Redux, WordPress, PHP, HTML, CSS, Django, Express, and AWS services. I specialize in creating modern, responsive UIs with Material UI and leveraging AWS for scalable and reliable applications. With a strong commitment to continuous learning and agile methodologies like SCRUM, I excel in both individual and team environments, delivering high-quality solutions efficiently.\nLet's connect and explore how I can bring value to your next project!",
+    description: "I'm a Full Stack Developer with expertise in JavaScript, React, Redux, WordPress, PHP, HTML, CSS, MUI, Django, Express, and AWS services. I specialize in creating modern, responsive UIs and leveraging AWS for scalable and reliable applications. With a strong commitment to continuous learning and agile methodologies like SCRUM, I excel in both individual and team environments, delivering high-quality solutions efficiently.\nLet's connect and explore how I can bring value to your next project!",
     arrowLink: "./#projects",
     arrowIcon: "/images/arrow.png",
 };
