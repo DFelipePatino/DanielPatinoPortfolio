7b84aa3d4c3b54f943f7767ae3b05cd4419a0b43
Daniel Patino <dafepa10c29@gmail.com>
2026-04-10 16:16:05 -0500

    expense tracker updated

--- src/OOPData/Projects-data.jsx
+++ src/OOPData/Projects-data.jsx
@@ -34,11 +34,11 @@ export const PROJECTS = [
     {
         id: 12,
         title: "Expense Tracker",
-        image: "/images/Expense Tracker.jpeg",
-        image2: "/images/Expense Tracker.jpeg",
+        image: "/images/Expense Tracker Mobile.jpeg",
+        image2: "/images/Expense Tracker Web.jpeg",
         imageAlt: "Project 2",
-        demoLink: "",
-        demoLink2: "exp://u.expo.dev/c7bc8080-3365-496c-9e02-7d2235851151/group/b0692d29-bc0c-41b0-a3d6-479d50b9e3cb",
+        demoLink: "https://expenses-app-dere.onrender.com",
+        demoLink2: "exp://u.expo.dev/c7bc8080-3365-496c-9e02-7d2235851151/group/bc427d28-6e15-4591-9d5a-44bcd05f8c93",
         description: "Expense Tracker is a React Native financial engine designed to automate complex property management by calculating a payout from gross rent to net individual profit. It features a bilingual UI, offline-first SQLite storage, and a custom Google Sheets bridge to seamlessly handle shared expenses and internal rent transfers.",
         buttons: [
             {
